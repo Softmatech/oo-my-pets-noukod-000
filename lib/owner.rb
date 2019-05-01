a806380b86e53b12dc598ab1240c1608adf36517
Andy Feidje Joseph <ingandy75@gmail.com>
2019-05-01 02:26:44 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -46,12 +46,13 @@ end
 def walk_dogs
   @pets[:dogs].each do |dog|
     dog.mood = "happy"
+  end
 end
 
 def play_with_cats
   @pets[:cats].each do |cat|
     cat.mood = "happy"
-end
+  end
 end
 
 def feed_fish
